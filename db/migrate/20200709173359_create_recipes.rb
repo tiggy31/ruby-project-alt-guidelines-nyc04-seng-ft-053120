8ef7ce5f4be9db5bb3ba4c56ce07b98f2e1730d3
tiggy31 <tigist_alemu@fitnyc.edu>
2020-07-10 09:56:43 -0400

    final

---
+++ db/migrate/20200709173359_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.integer :user_id
+      t.integer :drink_id
+      t.string  :ingredients
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200709173435_create_saveddrinks.rb
@@ -0,0 +1,5 @@
+class CreateSaveddrinks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :drinks, :created_by, :string
+  end
+end
